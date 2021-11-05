549629c3bc2dcdf07aaa49feee902e7ebec24512
USER-20200412YA\Administrator <xh411200015@163.com>
2021-11-05 22:57:37 +0800

    fix ignore

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -3,18 +3,29 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen
+        self.ai_settings = ai_settings
 
         self.images = pygame.image.load('images/ship.bmp')
         self.rect = self.images.get_rect()
         self.screen_rect = screen.get_rect()
 
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottomgit
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.moving_right = False
+        self.moving_left = False
+
+        self.center = float(self.rect.centerx)
+
+    def update(self):
+        if self.moving_right and self.rect.right<self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left and self.rect.left >0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
 
     def biltme(self):
         self.screen.blit(self.images, self.rect)
-
-
-
