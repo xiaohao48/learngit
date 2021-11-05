e70ac142f1a5f11ea33474b2d94aebfc5b16b569
USER-20200412YA\Administrator <xh411200015@163.com>
2021-11-05 22:34:07 +0800

    add ship

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -11,7 +11,7 @@ class Ship():
         self.screen_rect = screen.get_rect()
 
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.bottomgit
 
     def biltme(self):
         self.screen.blit(self.images, self.rect)
