e327f49069a35254c16ebd9ce72f7c008632c0f4
X-hao <xh411200015@163.com>
2022-02-26 14:23:00 +0800

    获取文件名

--- GUI/02.extract pdf form/test001.py
+++ GUI/02.extract pdf form/test001.py
@@ -1,17 +1,14 @@
 from tkinter import *
 from tkinter.filedialog import *
 
-root = Tk()
+root = Tk();
 root.geometry("400x100")
 
 
 def test1():
-    f = askopenfilename(title="上传文件", initialdir="f:/file")
-
-
-    print(f)
-    show["text"] = f
-Button(root, text="选择编辑的视频文件", command=test1).pack()
-show = Label(root, width=40, height=3, bg="green")
+    f = askopenfilename(title="上传文件")  # print(f)
+    show["text"]=f
+Button(root,text="选择编辑的视频文件",command=test1).pack()
+show = Label(root,width=40,height=3,bg="green")
 show.pack()
 root.mainloop()
--- GUI/02.extract pdf form/tool_pdf.py
+++ GUI/02.extract pdf form/tool_pdf.py
@@ -1,24 +1,32 @@
 import tkinter as tk
+from tkinter import filedialog
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.createWidget()
+def create_widget(root):
+    create_label(root, "请选择文件", 10, 10, 10, 2, 'green')
+    create_button(root, "选择文件", ask_file, 10, 50)
 
-    def createWidget(self):
-        self.createLabel("请选择文件", row=0, column=0, rowspan=1, columnspan=1)
 
-    def createLabel(self, text, row, column, rowspan, columnspan):
-        self.create_label = tk.Label(self, text=text)
-        self.create_label.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan)
+def create_label(root, text, x, y, width, height, bg):
+    create_new_label = tk.Label(root, text=text, width=width, height=height, bg=bg)
+    create_new_label.place(x=x, y=y)
+    return create_new_label
+
+
+def create_button(root, text, command, x, y):
+    create_new_button = tk.Button(root, text=text, command=command)
+    create_new_button.place(x=x, y=y)
+
+
+def ask_file():
+    f = filedialog.askopenfilename(title='选择文件')
+    show = create_label(root, '', 10, 90, 50, 2, 'pink')
+    show["text"] = f
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.geometry('1200x800+50+50')
+    root.geometry('500x300')
     root.title("pdf tools")
-    app = Application(master=root)
+    create_widget(root)
     root.mainloop()
