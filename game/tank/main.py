032d35745dd195f4b06100f2e81bb7b0d5ee8264
X-hao <xh411200015@163.com>
2022-05-16 18:39:27 +0800

    添加坦克移动

--- game/tank/main.py
+++ game/tank/main.py
@@ -1,4 +1,5 @@
-#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+import time
+
 import pygame
 from pygame.locals import *
 import sys
@@ -18,42 +19,103 @@ class MainGame():
     SCREEN_HEIGHT = 500
     SCREEN_WIDTH = 800
 
+    TANK_P1 = None
+
     def __init__(self):
         ...
 
     def startGame(self):
-        ...
         pygame.display.init()  # 初始化游戏窗口
-        screen = pygame.display.set_mode([MainGame.SCREEN_WIDTH, MainGame.SCREEN_HEIGHT])  # 加载窗口
+        MainGame.window = pygame.display.set_mode([MainGame.SCREEN_WIDTH, MainGame.SCREEN_HEIGHT])  # 加载窗口
         pygame.display.set_caption("坦克大战" + version)  # 设置标题
+
+        MainGame.TANK_P1 = Tank(400, 300, 5)
+
         while True:
-            screen.fill(BLACK)
-            screen.blit(self.getTextSurface(f"剩余敌方坦克5辆{version}"), (5, 5))
-            self.checkEvent()
+            MainGame.window.fill(BLACK)
+            MainGame.window.blit(self.getTextSurface(f"剩余敌方坦克5辆{version}"), (5, 5))
+            self.getEvent()
+
+            MainGame.TANK_P1.displayTank()
+            if not MainGame.TANK_P1.stop:
+                MainGame.TANK_P1.move()
+            time.sleep(0.02)
+
             pygame.display.update()
 
     def endGame(self):
-        ...
+        print("谢谢使用")
+        exit()
 
     def getTextSurface(self, text):
         pygame.font.init()
         font = pygame.font.SysFont('kaiti', 18)
         textSurface = font.render(text, True, RED)
         return textSurface
+
     def checkEvent(self):
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
 
-
+    def getEvent(self):
+        eventList = pygame.event.get()
+        for event in eventList:
+            if event.type == pygame.QUIT:
+                self.endGame()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    MainGame.TANK_P1.direction = "U"
+                    MainGame.TANK_P1.stop = False
+                    print("向上移动")
+                elif event.key == pygame.K_DOWN:
+                    MainGame.TANK_P1.direction = "D"
+                    MainGame.TANK_P1.stop = False
+                    print("向下移动")
+                elif event.key == pygame.K_LEFT:
+                    MainGame.TANK_P1.direction = "L"
+                    MainGame.TANK_P1.stop = False
+                    print("向左移动")
+                elif event.key == pygame.K_RIGHT:
+                    MainGame.TANK_P1.direction = "R"
+                    MainGame.TANK_P1.stop = False
+                    print("向右移动")
+                elif event.key == pygame.K_SPACE:
+                    print("发射子弹")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    MainGame.TANK_P1.stop = True
 
 
 class Tank():
-    def __init__(self):
-        ...
+    def __init__(self, left, top, speed):
+        self.images = {
+            'U': pygame.image.load('img/p1tankU.gif'),
+            'D': pygame.image.load('img/p1tankD.gif'),
+            'L': pygame.image.load('img/p1tankL.gif'),
+            'R': pygame.image.load('img/p1tankR.gif')
+        }
+        self.direction = 'U'
+        self.image = self.images[self.direction]
+        self.rect = self.image.get_rect()
+        self.rect.left = left
+        self.rect.top = top
+        self.speed = speed
+        self.stop = True
 
     def move(self):
-        ...
+        if self.direction == "L":
+            if self.rect.left > 0:
+                self.rect.left -= self.speed
+        elif self.direction == "R":
+            if self.rect.left + self.rect.height < MainGame.SCREEN_WIDTH:
+                self.rect.left += self.speed
+        elif self.direction == "U":
+            if self.rect.top > 0:
+                self.rect.top -= self.speed
+        elif self.direction == "D":
+            if self.rect.top + self.rect.height < MainGame.SCREEN_HEIGHT:
+                self.rect.top += self.speed
 
     def hitWalls(self):
         ...
@@ -62,7 +124,8 @@ class Tank():
         ...
 
     def displayTank(self):
-        ...
+        self.image = self.images[self.direction]
+        MainGame.window.blit(self.image, self.rect)
 
 
 class MyTank(Tank):
