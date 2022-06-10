dcc0d0adcfc4f32357902b0288c588699ca817b6
X-hao <xh411200015@163.com>
2022-06-10 19:39:02 +0800

    aes加密

--- GUI/05UatasTool/main.py
+++ GUI/05UatasTool/main.py
@@ -3,10 +3,12 @@ import requests
 import json
 import pymysql
 from sshtunnel import SSHTunnelForwarder
+import paramiko
 
 order_no = None
 risk_loan_level = ['LEVEL_ONE', 'LEVEL_TWO', 'LEVEL_THREE']
 user_level = ['L-0', 'L-1']
+system = ['uatas test', 'zeus test', 'uatasfly test']
 
 uatas_mysql_config = {
     'host': '127.0.0.1',
@@ -21,29 +23,34 @@ SSH_mysql_config = {
     'ssh_username': 'xiaohao',
     'remote_bind_address': ('149.129.214.137', 3306)
 }
+proxies = {
+    "http": 'http://127.0.0.1:1080'
+    # "https": 'https://vhk.toolscash.top:52999'
+}
 
 
 def aes_encrypt():
     """AES加密"""
-    print(order_no.get(), order_no.get(), risk_loan_level_var.get(), user_level_var.get(), order_no.get())
-    url = 'http://tool.chacuo.net/cryptaes/'
+    url = 'https://tool.lmeee.com/jiami/crypt128inter'
     data = {
-        # 'data': '{"code":0,"data":{"code":200,"feature_list":[],"forbidApplyUntil":1647746861000,"loan_type":0,"order_no":"%s","partner_id":9000,"passed":"false","risk_amount":"0","risk_loan_level":"%s","sign":"4e32c73dafe476209843fd916a5cccb0","user_idcard":"1141210809980028","user_level":"%s","user_name":"twenty seven","user_phone":"82112341028","zeus_order_no":"%s"},"msg":""}' % (
-            # order_no.get(), risk_loan_level_var.get(), user_level_var.get(), order_no.get()),
-        'data': {"code":0,"data":{"code":200,"feature_list":[],"forbidApplyUntil":1647746861000,"loan_type":0,"order_no":"275765552729264","partner_id":9000,"passed":"false","risk_amount":"0","risk_loan_level":"LEVEL_ONE","sign":"4e32c73dafe476209843fd916a5cccb0","user_idcard":"1141210809980028","user_level":"L-0","user_name":"twenty seven","user_phone":"82112341028","zeus_order_no":"275765552729264"},"msg":""},
-        'type': 'aes',
-        'arg': 'm=cbc_pad=pkcs5_block=128_p=uaRZnvZoJZ8KSTuE_i=IFwDpMo01xY05ovH_o=0_s=gb2312_t=0'
+        "mode": "CBC",
+        "padding": "pkcs5",
+        "block": 128,
+        "password": "uaRZnvZoJZ8KSTuE",
+        "iv": "IFwDpMo01xY05ovH",
+        "encode": "base64",
+        "way": 1,
+        "text": "{\"code\":0,\"data\":{\"code\":200,\"feature_list\":[],\"forbidApplyUntil\":1647746861000,\"loan_type\":0,\"order_no\":\"%s\",\"partner_id\":9000,\"passed\":\"false\",\"risk_amount\":\"0\",\"risk_loan_level\":\"%s\",\"sign\":\"4e32c73dafe476209843fd916a5cccb0\",\"user_idcard\":\"1141210809980028\",\"user_level\":\"%s\",\"user_name\":\"twenty seven\",\"user_phone\":\"82112341028\",\"zeus_order_no\":\"%s\"},\"msg\":\"\"}"%(order_no.get(),risk_loan_level_var.get(),user_level_var.get(),order_no.get()),
+        "method": "aes"
     }
-    # show_lb['text'] = data
-    print(data)
-    result = requests.post(url, data)
-    print(result)
-    html = result.text
-    print(html)
-    # encrypt_date = json.loads(html)['data']
-    # print(encrypt_date)
-    # show_lb['text'] = encrypt_date
-    # return encrypt_date
+    try:
+        result = requests.post(url=url, data=data)
+        html = result.text
+        encrypt_date = json.loads(html)['d']['r']
+        show_lb['text'] = encrypt_date
+        return encrypt_date
+    except:
+        show_lb['text'] = f'{order_no.get()}加密失败'
 
 
 def mock_cloudun_callback():
@@ -67,8 +74,8 @@ def control_request():
     # print(html)
 
 
-def modify_machine_status():
-    """修改机审状态"""
+def ssh_sever_start():
+    global ssh_sever
     ssh_sever = SSHTunnelForwarder(
         ('jump-sz1.toolscash.top', 22201),
         ssh_password='rOub8bWwc3mxhpjj',
@@ -76,6 +83,16 @@ def modify_machine_status():
         remote_bind_address=('149.129.214.137', 3306)
     )
     ssh_sever.start()
+
+
+def ssh_sever_end():
+    db.close()
+    ssh_sever.close()
+
+
+def mysql_connect():
+    global db
+    ssh_sever_start()
     db = pymysql.connect(
         host='127.0.0.1',
         port=ssh_sever.local_bind_port,
@@ -84,14 +101,82 @@ def modify_machine_status():
         db='cash_order',
         charset='utf8'
     )
+    return db
+
+
+def mysql_select(db, sql):
     cursor = db.cursor()
-    sql = 'select version();'
-    cursor.execute(sql)
-    data = cursor.fetchone()
-    print(data)
-    show_lb['text'] = data
-    db.close()
-    ssh_sever.close()
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        show_lb['text'] = result
+        return result
+    except:
+        show_lb['text'] = f'{order_no.get()}查询错误'
+        return
+
+
+def mysql_modify(db, sql):
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        db.commit()
+        show_lb['text'] = f'{order_no.get()}修改成功'
+    except:
+        db.rollback()
+        show_lb['text'] = f'{order_no.get()}修改失败'
+
+
+def modify_machine_status():
+    """修改机审状态"""
+    db = mysql_connect()
+    sqls = [
+        f'UPDATE cash_order.orders as o SET o.status=45 WHERE o.order_no={order_no.get()};',
+        f'UPDATE cash_approve.approve as a SET a.machine_status=1 WHERE a.order_no={order_no.get()};']
+    for sql in sqls:
+        mysql_modify(db, sql)
+    ssh_sever_end()
+
+
+def order_change_reloan():
+    """修改订单状态未待重新放款"""
+    db = mysql_connect()
+    sqls = [
+        f'UPDATE cash_order.orders as o SET o.`status`=85 WHERE o.order_no={order_no.get()};',
+        f'UPDATE cash_core.loan as l SET l.`status`=3 WHERE l.order_No={order_no.get()};',
+        f'UPDATE cash_core.pay_record_log as g SET g.order_status=3 WHERE g.loan_id in (SELECT l.id FROM cash_core.loan as l WHERE l.order_No={order_no.get()});',
+        f'UPDATE cash_pay.timepay_loan_order as p SET p.`status`=3 WHERE p.loan_no ={order_no.get()};'
+    ]
+    for sql in sqls:
+        mysql_modify(db, sql)
+    ssh_sever_end()
+
+
+def order_overdue_sql():
+    """修改订单逾期"""
+    db = mysql_connect()
+    sqls = [
+        f'DELETE FROM `cash_core`.`day_order_acc` WHERE `loan_id` in (select id from cash_core.`loan` where order_No ={order_no.get()});',
+        f'UPDATE cash_core.`loan` SET loan_time =loan_time-86400*1 where order_No={order_no.get()};',
+        f'UPDATE cash_core.repayment_plan_detail SET  due_time=due_time-86400*1 where loan_id in (select id from cash_core.`loan` where order_No = {order_no.get()});'
+    ]
+    for sql in sqls:
+        mysql_modify(db, sql)
+    ssh_sever_end()
+
+
+def order_overdue_ssh():
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname='jump-sz1.toolscash.top', port=22203, username='xiaohao', password='rOub8bWwc3mxhpjj')
+    stdin, stdout, stderr = client.exec_command('df -h')
+    print(stdout.read().decode('utf-8'))
+    client.close()
+
+
+def clear_data():
+    order_no.set(value='')
+    show_lb['text'] = '已清空'
 
 
 if __name__ == '__main__':
@@ -99,6 +184,7 @@ if __name__ == '__main__':
     root.geometry('900x600')
     root.title('uatas tool')
 
+    """第一行"""
     frame01 = tk.Frame(master=root)
     frame01.pack()
     # 订单号标签
@@ -108,8 +194,14 @@ if __name__ == '__main__':
     order_no = tk.StringVar(frame01)
     order_no_entry = tk.Entry(frame01, text=order_no)
     order_no_entry.pack(side='left')
+    # 清空按钮
+    clear_bt = tk.Button(frame01, text='清空', command=clear_data)
+    clear_bt.pack(side='left')
     # 系统选择
-
+    system_var = tk.StringVar(value='uatas test')
+    system_op = tk.OptionMenu(frame01, system_var, *system)
+    system_op.pack(side='left')
+    """第二行"""
     frame02 = tk.Frame(master=root)
     frame02.pack()
     # risk_loan_level选择
@@ -124,17 +216,27 @@ if __name__ == '__main__':
     control_request_bt = tk.Button(frame02, text='请求风控', command=control_request)
     control_request_bt.pack(side='left')
     # 加密按钮
-    control_callback_bt = tk.Button(frame02, text='模拟cloudun回调', command=aes_encrypt)
+    aes_encrypt_bt = tk.Button(frame02, text='AES加密', command=aes_encrypt)
+    aes_encrypt_bt.pack(side='left')
+    control_callback_bt = tk.Button(frame02, text='模拟cloudun回调', command=control_request)
     control_callback_bt.pack(side='left')
-
+    """第三行"""
     frame03 = tk.Frame(master=root)
     frame03.pack()
     # 订单状态改为过机审
     machine_status_bt = tk.Button(frame03, text='订单过机审', command=modify_machine_status)
     machine_status_bt.pack(side='left')
     # 订单改为待重新放款
-
-    # 订单逾期
+    reloan_bt = tk.Button(frame03, text='订单待重新放款', command=order_change_reloan)
+    reloan_bt.pack(side='left')
+    # 订单逾期sql
+    order_overdue_bt = tk.Button(frame03, text='订单逾期', command=order_overdue_sql)
+    order_overdue_bt.pack(side='left')
+    # 订单逾期脚本
+    order_overdue_ssh_bt = tk.Button(frame03, text='订单逾期脚本', command=order_overdue_ssh)
+    order_overdue_ssh_bt.pack(side='left')
+    # 订单进贷后
+    # 订单进催收
 
     # 根据uid查询在storage哪个表
 
