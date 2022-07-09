48770712477eb7a849ab4ec81e20c3693e273404
X-hao <xh411200015@163.com>
2022-07-09 18:46:33 +0800

    优化脚本信息

--- GUI/05UatasTool/uatas_tool.py
+++ GUI/05UatasTool/uatas_tool.py
@@ -33,21 +33,48 @@ proxies = {
 }
 aes_key_iv = {
     "uatas_rc": {"key": "uaRZnvZoJZ8KSTuE", "iv": "IFwDpMo01xY05ovH"},  # uatas风控
-    "front_end_zeus": {"key": "THi8cml8EPlcdkfh", "iv": "1Q2ik8vpod90lhTg"},  # zeus前端
-    "back_end_zeus": {"key": "k169flxzbooneepf", "iv": "abc123rty456nji7"},  # zeus后台
+    "front_end_zeus": {"key": "Ckgx3U1QufHbcQns", "iv": "so8RhHi4jkaci4ze"},  # zeus前端
+    "back_end_zeus": {"key": "93LJ7sxQALXuMgLj", "iv": "jlsfjiosmhosl5db"},  # zeus后台
     "front_end_uatas": {"key": "daDtbXYkhhnHi1XF", "iv": "H9Rlows19wkBSx3w"},  # uatas前端
     "back_end_uatas": {"key": "z1aXNVB4JJw4ZZ0r", "iv": "LzsCmdfFbAvQ1fbD"}  # uatas后台
 }
 scripts = {
     '展期生成新订单': 'php /home/rong/www/time-core/webroot/batch.php push VirtualOrderNotify',
     'uatas放款拉取': "php /home/rong/www/time-pay/webroot/batch.php TimePayLoan PollingLoanResult --payHandel='UP'",
-    'uatas还款拉取': "php /home/rong/www/time-pay/webroot/batch.php TimePayRepay PollingRepayResult --orderNo='833487713322312' --payHandel='UP'",
-    "uatas入贷后": "php /home/rong/www/time-cuishou/webroot/batch.php Daihouivr GetDaihouIvr --day=1",
+    'uatas还款拉取': "php /home/rong/www/time-pay/webroot/batch.php TimePayRepay PollingRepayResult --orderNo='' --payHandel='UP'",
+    "uatas入贷后": "php /home/rong/www/time-cuishou/webroot/batch.php Daihouivr GetDaihouIvr --day=",
     "uatas逾期费": "php /home/rong/www/time-core/webroot/batch.php FqRepayment CalDayFee --loanId=",
     "uatas入催收": "php /home/rong/www/time-core/webroot/batch.php RepayPlanTask IncuiShouTask",
-    "用户storage目录查询": 'php -r "echo crc32(7044) % 10;"',
+    "用户storage目录查询": 'php -r "echo crc32() % 10;"',
     "uatas api绑定银行卡": "php /home/rong/www/time-order/webroot/command.php Customer ConsumeBankCard",
+    "oppo打包放款": "php /home/rong/UatasPay/yii batch-disbursement/batch-loan"
 }
+
+encrypt_list = [
+    ['zeus前端加密', {'key': 'Ckgx3U1QufHbcQns', 'iv': 'so8RhHi4jkaci4ze', 'way': 1}],
+    ['zeus前端解密', {'key': 'Ckgx3U1QufHbcQns', 'iv': 'so8RhHi4jkaci4ze', 'way': 2}],
+    ['zeus后端加密', {'key': '93LJ7sxQALXuMgLj', 'iv': 'jlsfjiosmhosl5db', 'way': 1}],
+    ['zeus后端解密', {'key': '93LJ7sxQALXuMgLj', 'iv': 'jlsfjiosmhosl5db', 'way': 2}],
+    ['uatas前端加密', {'key': 'daDtbXYkhhnHi1XF', 'iv': 'H9Rlows19wkBSx3w', 'way': 1}],
+    ['uatas前端解密', {'key': 'daDtbXYkhhnHi1XF', 'iv': 'H9Rlows19wkBSx3w', 'way': 2}],
+    ['uatas后端加密', {'key': 'z1aXNVB4JJw4ZZ0r', 'iv': 'LzsCmdfFbAvQ1fbD', 'way': 1}],
+    ['uatas后端解密', {'key': 'z1aXNVB4JJw4ZZ0r', 'iv': 'LzsCmdfFbAvQ1fbD', 'way': 2}]
+]
+encrypt_dict = dict(encrypt_list)
+
+
+def data_encrypt():
+    """业务加解密功能"""
+    # encrypt_dict = dict(encrypt_list)
+    if encrypt_var.get() in encrypt_dict.keys():
+        data = aes_encrypt(
+            url=url_crypt,
+            password=encrypt_dict[encrypt_var.get()]['key'],
+            iv=encrypt_dict[encrypt_var.get()]['iv'],
+            way=encrypt_dict[encrypt_var.get()]['way'],
+            text=data_encrypt_text.get(1.0, tk.END)
+        )
+        show_lb['text'] = data
 
 
 def aes_encrypt(url, password, iv, way, text):
@@ -74,13 +101,13 @@ def aes_encrypt(url, password, iv, way, text):
 
 
 def aes_rc():
-    url = 'https://tool.lmeee.com/jiami/crypt128inter'
     password = aes_key_iv['uatas_rc']['key']
     iv = aes_key_iv['uatas_rc']['iv']
     way = 1
     text = "{\"code\":0,\"data\":{\"code\":200,\"feature_list\":[],\"forbidApplyUntil\":1647746861000,\"loan_type\":0,\"order_no\":\"%s\",\"partner_id\":9000,\"passed\":\"false\",\"risk_amount\":\"0\",\"risk_loan_level\":\"%s\",\"sign\":\"4e32c73dafe476209843fd916a5cccb0\",\"user_idcard\":\"1141210809980028\",\"user_level\":\"%s\",\"user_name\":\"twenty seven\",\"user_phone\":\"82112341028\",\"zeus_order_no\":\"%s\"},\"msg\":\"\"}" % (
         order_no.get(), risk_loan_level_var.get(), user_level_var.get(), order_no.get())
-    aes_encrypt(url, password, iv, way, text)
+    aes = aes_encrypt(url_crypt, password, iv, way, text)
+    return aes
 
 
 # def front_end_encrypt():
@@ -126,7 +153,7 @@ def aes_rc():
 def mock_cloudun_callback():
     """模拟cloudun风控回调"""
     url = 'http://test-rc.uatas.id/rc/decisions/cloudun'
-    data = {'data': "%s" % (aes_rc())}
+    data = json.dumps({"data": "%s" % (aes_rc())})
     try:
         result = requests.post(url, data, proxies=proxies)
         html = result.text
@@ -137,13 +164,15 @@ def mock_cloudun_callback():
 
 def control_request():
     """订单请求风控"""
-    sql = f'UPDATE cash_order.orders as o SET o.create_time=o.create_time-3600 WHERE o.order_no={order_no.get()};'
+    sql = f'UPDATE cash_order.orders as o SET o.create_time=FROM_UNIXTIME(UNIX_TIMESTAMP(o.create_time)-3600) ' \
+          f'WHERE o.order_no={order_no.get()};'
     mysql_modify(mysql_connect(), sql)
 
-    time.sleep(2)
+    # time.sleep(2)
     ssh_sever_end()
     url = 'http://test-rc.uatas.id/rc/check'
     data = {'data': '{"order_no": "%s" }' % order_no.get()}
+    print(data)
     try:
         result = requests.post(url, data)
         html = result.text
@@ -153,6 +182,7 @@ def control_request():
 
 
 def ssh_sever_start():
+    """跳板服务开启"""
     global ssh_sever
     ssh_sever = SSHTunnelForwarder(
         ('jump-sz1.toolscash.top', 22201),
@@ -164,11 +194,13 @@ def ssh_sever_start():
 
 
 def ssh_sever_end():
+    """跳板服务关闭"""
     db.close()
     ssh_sever.close()
 
 
 def mysql_connect():
+    """数据库连接"""
     global db
     ssh_sever_start()
     db = pymysql.connect(
@@ -183,6 +215,7 @@ def mysql_connect():
 
 
 def mysql_select(db, sql):
+    """数据库查询操作"""
     cursor = db.cursor()
     try:
         cursor.execute(sql)
@@ -194,6 +227,7 @@ def mysql_select(db, sql):
 
 
 def mysql_modify(db, sql):
+    """数据库更新操作"""
     cursor = db.cursor()
     try:
         cursor.execute(sql)
@@ -205,6 +239,7 @@ def mysql_modify(db, sql):
 
 
 def select_order():
+    """数据库订单状态查询"""
     db = mysql_connect()
     sql = f'SELECT o.order_no,o.`status` FROM cash_order.orders as o WHERE o.order_no ={order_no.get()};'
     mysql_select(db, sql)
@@ -243,7 +278,8 @@ def order_overdue_sql():
     sqls = [
         f'DELETE FROM `cash_core`.`day_order_acc` WHERE `loan_id` in (select id from cash_core.`loan` where order_No ={order_no.get()});',
         f'UPDATE cash_core.`loan` SET loan_time =loan_time-86400*1 where order_No={order_no.get()};',
-        f'UPDATE cash_core.repayment_plan_detail SET  due_time=due_time-86400*1 where loan_id in (select id from cash_core.`loan` where order_No = {order_no.get()});'
+        f'UPDATE cash_core.repayment_plan_detail SET  due_time=due_time-86400*1 where loan_id in (select id from cash_core.`loan` where order_No = {order_no.get()});',
+        f'UPDATE cash_order.orders as o SET o.loan_time=o.loan_time-86400*1 WHERE o.order_no={order_no.get()};'
     ]
     for sql in sqls:
         mysql_modify(db, sql)
@@ -291,6 +327,7 @@ def order_overdue_ssh():
 
 
 def clear_data():
+    """清空功能"""
     order_no.set(value='')
     # data_encrypt_var.set(value='')
     data_encrypt_text.delete(1.0, tk.END)
@@ -298,16 +335,23 @@ def clear_data():
 
 
 def copy():
+    """复制功能"""
     return pyperclip.copy(show_lb['text'])
 
 
 def create_button(master, text, command, row, column, width=30, sticky='ew'):
+    """创建按钮功能"""
     new_button = tk.Button(master=master, text=text, command=command, width=width)
     new_button.grid(row=row, column=column, sticky=sticky)
+    return new_button
 
 
 def script():
-    show_lb['text'] = scripts[script_var.get()]
+    """查看脚本"""
+    if script_var.get() in scripts.keys():
+        show_lb['text'] = scripts[script_var.get()]
+    else:
+        show_lb['text'] = '输入错误'
 
 
 if __name__ == '__main__':
@@ -373,7 +417,17 @@ if __name__ == '__main__':
     data_encrypt_text = tk.Text(root, width=120, height=4)
     data_encrypt_text.grid(row=row, column=column, columnspan=5)
 
+    """加解密行"""
+    row = row + 1
+    encrypt_lb = tk.Label(root, text='待加解密数据↑')
+    encrypt_lb.grid(row=row, column=column + 1)
+    encrypt_var = tk.StringVar(value=encrypt_list[0][0])
+    encrypt_op = tk.OptionMenu(root, encrypt_var, *encrypt_dict.keys())
+    encrypt_op.grid(row=row, column=column + 2, sticky='ew')
+    encrypt_bt = create_button(root, text='加解密', command=data_encrypt, row=row, column=column + 3)
+
     """zeus加解密行"""
+    """
     row = row + 1
     front_end_encrypt_bt_z = create_button(root, text="zeus前端加密", command=lambda: aes_encrypt(
         url=url_crypt,
@@ -404,7 +458,7 @@ if __name__ == '__main__':
         text=data_encrypt_text.get(1.0, tk.END)
     ), row=row, column=column + 4, width=24)
 
-    """uatas加解密行"""
+    '''uatas加解密行'''
     row = row + 1
     front_end_encrypt_bt_u = create_button(root, text="uatas前端加密", command=lambda: aes_encrypt(
         url=url_crypt,
@@ -434,10 +488,11 @@ if __name__ == '__main__':
         way=2,
         text=data_encrypt_text.get(1.0, tk.END)
     ), row=row, column=column + 4, width=24)
+    """
 
     """查看脚本"""
     row = row + 1
-    script_var = tk.StringVar()
+    script_var = tk.StringVar(value='选择脚本')
     script_op = tk.OptionMenu(root, script_var, *scripts.keys())
     script_op.grid(row=row, column=column + 1, sticky='ew')
     script_bt = create_button(master=root, text='查看脚本', row=row, column=column + 2, command=script)
