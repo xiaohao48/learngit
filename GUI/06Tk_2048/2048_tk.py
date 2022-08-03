af9dbdc3fb9537475345a4082dcb646b23cc6b42
X-hao <xh411200015@163.com>
2022-08-03 17:41:15 +0800

    优化随机函数

--- GUI/06Tk_2048/2048_tk.py
+++ GUI/06Tk_2048/2048_tk.py
@@ -32,96 +32,70 @@ fg_color = {
 rows = 2
 columns = 4
 squ_size = [rows, columns]
-num_list = [['' for m in range(columns)] for n in range(rows)]
+gridCell_label = []
+gridCell_num = [[0] * columns for n in range(rows)]
 
 
 def create_widget():
     create_squ()  # 创建游戏区域
-    init_random()  # 初始化随机生成2个数字
-
+    random_num()  # 初始化随机生成1个数字
+    random_num()  # 初始化随机生成1个数字
+    show_num()  # 展示数字
     root.bind('<Key>', move)  # 移动操作
 
 
 def create_squ():
     """使用label创建游戏区域"""
     for row in range(rows):
+        row_cells = []
         for column in range(columns):
-            num_list[row][column] = tk.Label(root, text='', bg='azure4', font=('arial', 22), width=4, height=2)
-            num_list[row][column].grid(row=row, column=column, padx=5, pady=5)
+            label = tk.Label(root, text='', bg='azure4', font=('arial', 22), width=4, height=2)
+            label.grid(row=row, column=column, padx=5, pady=5)
+            row_cells.append(label)
+        gridCell_label.append(row_cells)
 
 
 def random_num():
-    random_row = random.randint(1, rows)
-    random_column = random.randint(1, columns)
+    random_cells = []
+    for row in range(rows):
+        for column in range(columns):
+            if gridCell_num[row][column] == 0:
+                random_cells.append((row, column))
+    random_cell = random.choice(random_cells)
     random_text = random.choice([2, 4])
-    # num_list[random_row - 1][random_column - 1]['text'] = random_text
-    return [random_row, random_column, random_text, num_list[random_row - 1][random_column - 1]]
-
-
-def init_random():
-    """初始化随机两个2"""
-    random_list = []
-    while True:
-        random_1 = random_num()
-        random_2 = random_num()
-        print(random_1, random_2, sys._getframe().f_lineno)
-        if random_1[0] != random_2[0] or random_1[1] != random_2[1]:
-            random_list.append(random_1)
-            random_list.append(random_2)
-            # show_num()  # 展示数字
-            # print(random_list)
-        if len(random_list) == 2:
-            break
-        # print(random_list)
-    num_list[random_list[0][0] - 1][random_list[0][1] - 1]['text'] = random_list[0][2]
-    num_list[random_list[1][0] - 1][random_list[1][1] - 1]['text'] = random_list[1][2]
-    show_num()
-    print(random_list, sys._getframe().f_lineno)
+    gridCell_num[random_cell[0]][random_cell[1]] = random_text
+    print('row=' + str(random_cell[0]),
+          'columu=' + str(random_cell[1]),
+          'random_text=' + str(random_text),
+          'lineno=' + str(sys._getframe().f_lineno))
 
 
 def show_num():
     """展示数字颜色"""
     for row in range(rows):
         for column in range(columns):
-            if str(num_list[row][column]['text']) in bg_color.keys():
-                num_list[row][column].config(bg=bg_color[str(num_list[row][column]['text'])],
-                                             fg=fg_color[str(num_list[row][column]['text'])])
+            if gridCell_num[row][column] != 0:
+                gridCell_label[row][column].config(bg=bg_color[str(gridCell_num[row][column])],
+                                                   fg=fg_color[str(gridCell_num[row][column])],
+                                                   text=gridCell_num[row][column])
             else:
-                num_list[row][column].config(bg='azure4')
-
-
-def create_new_grid():
-    null_num = []
-    for row in range(rows):
-        for column in range(columns):
-            if num_list[row][column]['text'] == '':
-                null_num.append((row, column))
-    if len(null_num) > 0:
-        random_null = random.choice(null_num)
-        random_text = random.choice([2, 4])
-        num_list[random_null[0]][random_null[1]]['text'] = random_text
-        null_num.remove(random_null)
-        print(random_null)
-        show_num()  # 展示数字
-    print(null_num)
-
-
-def create_new_grid2():
-    ...
+                gridCell_label[row][column].config(bg='azure4')
 
 
 def move(event):
     derictions = {
         'Up': "print('up')",
         'Down': "print('down')",
-        'Left': left(),
+        'Left': left,
         'Right': "print('right')",
     }
     # sys._getframe().f_lineno
-    print(event.keysym, sys._getframe().f_lineno)
+    pressed_key = event.keysym
+    print(pressed_key, event.keysym, sys._getframe().f_lineno)
     if event.keysym in derictions.keys():
-        # print(derictions[event.keysym])
+        print(derictions[event.keysym])
         derictions[event.keysym]
+
 
 
 def left():
@@ -129,12 +103,12 @@ def left():
         # for column in range(0, columns - 1):
         column = 0
         while column < columns - 1:
-            print(str(num_list[row][column]['text']))
-            if str(num_list[row][column]['text']) == '':
-                num_list[row][column]['text'], num_list[row][column + 1]['text'] = num_list[row][column + 1]['text'], \
-                                                                                   num_list[row][column]['text']
+            print(str(gridCell_num[row][column]), sys._getframe().f_lineno)
+            if str(gridCell_num[row][column]) == '':
+                gridCell_num[row][column], gridCell_num[row][column + 1] = gridCell_num[row][column + 1], \
+                                                                           gridCell_num[row][column]
                 show_num()
-                print(num_list[row][column]['text'])
+                print(gridCell_num[row][column], sys._getframe().f_lineno)
                 column += 1
             else:
                 break
